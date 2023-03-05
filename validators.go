69ad0fa4dfb1252bc7455ec2f17151b9afef6b80
Dan Chapman <dan@Daniels-MacBook-Air.local>
2023-03-05 11:15:07 -0500

    use errors.Join instead of run

--- validators.go
+++ validators.go
@@ -7,64 +7,50 @@ import (
     "strconv"
 )
 
-type Rule func() error
-
 // Between checks if a value is between min and max.
-func Between[T constraints.Ordered](name string, value, min, max T) Rule {
-    return func() error {
-        if value < min || value > max {
-            return errors.New(name + " is out of range")
-        }
-        return nil
+func Between[T constraints.Ordered](name string, value, min, max T) error {
+    if value < min || value > max {
+        return errors.New(name + " is out of range")
     }
+    return nil
 }
 
 // Exists checks if a value is not nil.
-func Exists(name string, value interface{}) Rule {
-    return func() error {
-        if value == nil {
-            return errors.New(name + " is required")
-        }
-        return nil
+func Exists(name string, value interface{}) error {
+    if value == nil {
+        return errors.New(name + " is required")
     }
+    return nil
 }
 
 // IsInt checks if a string value is an integer.
-func IsInt(name string, value string, bitSize int) Rule {
-    return func() error {
-        if _, err := strconv.ParseInt(value, 10, bitSize); err != nil {
-            return errors.New(name + " is not an integer")
-        }
-        return nil
+func IsInt(name string, value string, bitSize int) error {
+    if _, err := strconv.ParseInt(value, 10, bitSize); err != nil {
+        return errors.New(name + " is not an integer")
     }
+    return nil
 }
 
 // Length checks if a slice is of a certain length.
-func Length[T any](name string, value []T, max int) Rule {
-    return func() error {
-        if len(value) > max {
-            return errors.New(name + " is out of range")
-        }
-        return nil
+func Length[T any](name string, value []T, max int) error {
+    if len(value) > max {
+        return errors.New(name + " is out of range")
     }
+    return nil
 }
 
 // Matches checks if a string value matches a regular expression.
-func Matches(name string, value string, regex *regexp.Regexp) Rule {
-    return func() error {
-        if !regex.MatchString(value) {
-            return errors.New(name + " is invalid")
-        }
-        return nil
+func Matches(name string, value string, regex *regexp.Regexp) error {
+    if !regex.MatchString(value) {
+        return errors.New(name + " is invalid")
     }
+    return nil
 }
 
 // NotEmpty checks if a slice is not empty.
-func NotEmpty(name string, value []interface{}) Rule {
-    return func() error {
-        if len(value) == 0 {
-            return errors.New(name + " is required")
-        }
-        return nil
+func NotEmpty[T any](name string, value []T) error {
+    if len(value) == 0 {
+        return errors.New(name + " is required")
     }
+    return nil
 }
--- validators_test.go
+++ validators_test.go
@@ -1,185 +1,224 @@
 package validate
 
 import (
-	"regexp"
-	"testing"
+    "errors"
+    "regexp"
+    "testing"
 )
 
 func Test_Between_Int(t *testing.T) {
-	testcases := []struct {
-		test      string
-		name      string
-		value     int
-		min       int
-		max       int
-		expectErr bool
-	}{
-		{"Valid", "value", 5, 1, 10, false},
-		{"Too Large", "value", 11, 1, 10, true},
-		{"Too Small", "value", 0, 1, 10, true},
-	}
+    testcases := []struct {
+        test      string
+        name      string
+        value     int
+        min       int
+        max       int
+        expectErr bool
+    }{
+        {"Valid", "value", 5, 1, 10, false},
+        {"Too Large", "value", 11, 1, 10, true},
+        {"Too Small", "value", 0, 1, 10, true},
+    }
 
-	for _, tc := range testcases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := Between(tc.name, tc.value, tc.min, tc.max)()
-			if tc.expectErr && err == nil {
-				t.Error("Expected error, got nil")
-			}
-			if !tc.expectErr && err != nil {
-				t.Errorf("Expected no error, got %v", err)
-			}
-		})
-	}
+    for _, tc := range testcases {
+        t.Run(tc.test, func(t *testing.T) {
+            err := Between(tc.name, tc.value, tc.min, tc.max)
+            if tc.expectErr && err == nil {
+                t.Error("Expected error, got nil")
+            }
+            if !tc.expectErr && err != nil {
+                t.Errorf("Expected no error, got %v", err)
+            }
+        })
+    }
 }
 
 func Test_Between_Float(t *testing.T) {
-	testcases := []struct {
-		test      string
-		name      string
-		value     float64
-		min       float64
-		max       float64
-		expectErr bool
-	}{
-		{"Valid", "value", 5, 1, 10, false},
-		{"Too Large", "value", 11, 1, 10, true},
-		{"Too Small", "value", 0, 1, 10, true},
-	}
+    testcases := []struct {
+        test      string
+        name      string
+        value     float64
+        min       float64
+        max       float64
+        expectErr bool
+    }{
+        {"Valid", "value", 5, 1, 10, false},
+        {"Too Large", "value", 11, 1, 10, true},
+        {"Too Small", "value", 0, 1, 10, true},
+    }
 
-	for _, tc := range testcases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := Between(tc.name, tc.value, tc.min, tc.max)()
-			if tc.expectErr && err == nil {
-				t.Error("Expected error, got nil")
-			}
-			if !tc.expectErr && err != nil {
-				t.Errorf("Expected no error, got %v", err)
-			}
-		})
-	}
+    for _, tc := range testcases {
+        t.Run(tc.test, func(t *testing.T) {
+            err := Between(tc.name, tc.value, tc.min, tc.max)
+            if tc.expectErr && err == nil {
+                t.Error("Expected error, got nil")
+            }
+            if !tc.expectErr && err != nil {
+                t.Errorf("Expected no error, got %v", err)
+            }
+        })
+    }
 }
 
 func Test_Exists(t *testing.T) {
-	testcases := []struct {
-		test      string
-		name      string
-		value     interface{}
-		expectErr bool
-	}{
-		{"Valid", "value", "test", false},
-		{"Invalid", "value", nil, true},
-	}
+    testcases := []struct {
+        test      string
+        name      string
+        value     interface{}
+        expectErr bool
+    }{
+        {"Valid", "value", "test", false},
+        {"Invalid", "value", nil, true},
+    }
 
-	for _, tc := range testcases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := Exists(tc.name, tc.value)()
-			if tc.expectErr && err == nil {
-				t.Error("Expected error, got nil")
-			}
-			if !tc.expectErr && err != nil {
-				t.Errorf("Expected no error, got %v", err)
-			}
-		})
-	}
+    for _, tc := range testcases {
+        t.Run(tc.test, func(t *testing.T) {
+            err := Exists(tc.name, tc.value)
+            if tc.expectErr && err == nil {
+                t.Error("Expected error, got nil")
+            }
+            if !tc.expectErr && err != nil {
+                t.Errorf("Expected no error, got %v", err)
+            }
+        })
+    }
 }
 
 func Test_IsInt64(t *testing.T) {
-	testcases := []struct {
-		test      string
-		name      string
-		value     string
-		bitSize   int
-		expectErr bool
-	}{
-		{"Valid", "value", "123", 32, false},
-		{"Invalid", "value", "test", 64, true},
-	}
+    testcases := []struct {
+        test      string
+        name      string
+        value     string
+        bitSize   int
+        expectErr bool
+    }{
+        {"Valid", "value", "123", 32, false},
+        {"Invalid", "value", "test", 64, true},
+    }
 
-	for _, tc := range testcases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := IsInt(tc.name, tc.value, tc.bitSize)()
-			if tc.expectErr && err == nil {
-				t.Error("Expected error, got nil")
-			}
-			if !tc.expectErr && err != nil {
-				t.Errorf("Expected no error, got %v", err)
-			}
-		})
-	}
+    for _, tc := range testcases {
+        t.Run(tc.test, func(t *testing.T) {
+            err := IsInt(tc.name, tc.value, tc.bitSize)
+            if tc.expectErr && err == nil {
+                t.Error("Expected error, got nil")
+            }
+            if !tc.expectErr && err != nil {
+                t.Errorf("Expected no error, got %v", err)
+            }
+        })
+    }
 }
 
 func Test_Matches(t *testing.T) {
-	testcases := []struct {
-		test      string
-		name      string
-		value     string
-		pattern   string
-		expectErr bool
-	}{
-		{"Valid", "value", "test", "t[a-z]st", false},
-		{"Invalid", "value", "test", "bad", true},
-	}
+    testcases := []struct {
+        test      string
+        name      string
+        value     string
+        pattern   string
+        expectErr bool
+    }{
+        {"Valid", "value", "test", "t[a-z]st", false},
+        {"Invalid", "value", "test", "bad", true},
+    }
 
-	for _, tc := range testcases {
-		t.Run(tc.test, func(t *testing.T) {
-			regexp, _ := regexp.Compile(tc.pattern)
-			err := Matches(tc.name, tc.value, regexp)()
-			if tc.expectErr && err == nil {
-				t.Error("Expected error, got nil")
-			}
-			if !tc.expectErr && err != nil {
-				t.Errorf("Expected no error, got %v", err)
-			}
-		})
-	}
+    for _, tc := range testcases {
+        t.Run(tc.test, func(t *testing.T) {
+            regexp, _ := regexp.Compile(tc.pattern)
+            err := Matches(tc.name, tc.value, regexp)
+            if tc.expectErr && err == nil {
+                t.Error("Expected error, got nil")
+            }
+            if !tc.expectErr && err != nil {
+                t.Errorf("Expected no error, got %v", err)
+            }
+        })
+    }
 }
 
 func Test_Length(t *testing.T) {
-	testcases := []struct {
-		test      string
-		name      string
-		value     []interface{}
-		max       int
-		expectErr bool
-	}{
-		{"Valid", "value", []interface{}{"test"}, 10, false},
-		{"Too Large", "value", []interface{}{"test", "test"}, 1, true},
-	}
+    testcases := []struct {
+        test      string
+        name      string
+        value     []interface{}
+        max       int
+        expectErr bool
+    }{
+        {"Valid", "value", []interface{}{"test"}, 10, false},
+        {"Too Large", "value", []interface{}{"test", "test"}, 1, true},
+    }
 
-	for _, tc := range testcases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := Length(tc.name, tc.value, tc.max)()
-			if tc.expectErr && err == nil {
-				t.Error("Expected error, got nil")
-			}
-			if !tc.expectErr && err != nil {
-				t.Errorf("Expected no error, got %v", err)
-			}
-		})
-	}
+    for _, tc := range testcases {
+        t.Run(tc.test, func(t *testing.T) {
+            err := Length(tc.name, tc.value, tc.max)
+            if tc.expectErr && err == nil {
+                t.Error("Expected error, got nil")
+            }
+            if !tc.expectErr && err != nil {
+                t.Errorf("Expected no error, got %v", err)
+            }
+        })
+    }
 }
 
 func Test_NotEmpty(t *testing.T) {
-	testcases := []struct {
-		test      string
-		name      string
-		value     []interface{}
-		expectErr bool
-	}{
-		{"Valid", "value", []interface{}{"test"}, false},
-		{"Invalid", "value", []interface{}{}, true},
-		{"Invalid", "value", nil, true},
-	}
+    testcases := []struct {
+        test      string
+        name      string
+        value     []interface{}
+        expectErr bool
+    }{
+        {"Valid", "value", []interface{}{"test"}, false},
+        {"Invalid", "value", []interface{}{}, true},
+        {"Invalid", "value", nil, true},
+    }
 
-	for _, tc := range testcases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := NotEmpty(tc.name, tc.value)()
-			if tc.expectErr && err == nil {
-				t.Error("Expected error, got nil")
-			}
-			if !tc.expectErr && err != nil {
-				t.Errorf("Expected no error, got %v", err)
-			}
-		})
-	}
+    for _, tc := range testcases {
+        t.Run(tc.test, func(t *testing.T) {
+            err := NotEmpty(tc.name, tc.value)
+            if tc.expectErr && err == nil {
+                t.Error("Expected error, got nil")
+            }
+            if !tc.expectErr && err != nil {
+                t.Errorf("Expected no error, got %v", err)
+            }
+        })
+    }
+}
+
+func Test_ValidationChain(t *testing.T) {
+    t.Run("No Errors", func(t *testing.T) {
+        err := errors.Join(
+            Exists("exists", "test"),
+            NotEmpty("notempty", []string{"test"}),
+            Between("between", 5, 1, 10),
+        )
+
+        if err != nil {
+            t.Error("Expected no error, got", err)
+        }
+    })
+
+    t.Run("One Error", func(t *testing.T) {
+        err := errors.Join(
+            Exists("exists", "test"),
+            NotEmpty("notempty", []string{}),
+            Between("between", 5, 1, 10),
+        )
+
+        if err == nil {
+            t.Error("Expected error")
+        }
+    })
+
+    t.Run("Many Error", func(t *testing.T) {
+        err := errors.Join(
+            Exists("exists", nil),
+            NotEmpty("notempty", []string{}),
+            Between("between", 11, 1, 10),
+        )
+
+        if err == nil {
+            t.Error("Expected error")
+        }
+    })
 }
